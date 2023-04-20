1d899eb60529c3e6924947e98495f910380913de
Taka <103984069+Taka0007@users.noreply.github.com>
2023-04-20 15:51:59 +0900

    Update no-restriction.m

--- Intro-to-optimization/no-restriction.m
+++ Intro-to-optimization/no-restriction.m
@@ -1,11 +1,18 @@
-% Task-1
 % 最適化問題の作成
 
 % prob という名前の最適化問題を作成し、これを説明するための情報として "Factory Location" を追加
 prob = optimproblem("Description", ...
     "Factory Location");
     
-   
-% Task-2
+
 % plot関数を使い、現在の最適化問題の様子をわかりやすく図示
 show(prob)
+
+
+% 最適化変数の定義 --> optimvar  (optimize variableから来てる？)
+% シンボリック最適化変数x,yを作成し、この変数に「x」「y」という名前を付ける
+prob = optimproblem("Description","Factory Location")
+x = optimvar("x")
+y = optimvar("y")
+
+
