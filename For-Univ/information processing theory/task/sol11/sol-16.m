dc0ceaa7e24e51c0355c6f93e2e53b0059023d55
Taka0007 <7takacode@gmail.com>
2023-07-04 15:52:21 +0900

    solve16

--- For-Univ/information processing theory/task/sol11/sol-16.m
+++ For-Univ/information processing theory/task/sol11/sol-16.m
@@ -1,11 +1,22 @@
 % 2次方程式の係数が与えられる。それをもとに解を求めるプログラム
 
-coef = randi([1,77],1,3);
+% 1-77までの整数の中からランダムに3つ選ぶ。
+% これを2次方程式の係数とする
+coeff = randi([1,77],1,3);
 disp('係数')
-coef
+disp(coeff)
 
+a = coeff(1);
+b = coeff(2);
+c = coeff(3);
 
+% 方程式の解
+Ans1 = (-b + sqrt(b^2-4*a*c)) / (2*a);
+Ans2 = (-b - sqrt(b^2-4*a*c)) / (2*a);
+disp('解答');
+disp(Ans1);
+disp(Ans2);
 
 % 検算用
-disp('検算用')
-roots(coef)
+disp('検算用');
+disp(roots(coeff));
