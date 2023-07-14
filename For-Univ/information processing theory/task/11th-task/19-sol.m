4d52ed585c6549a11c5705a88d42e76b95590147
Taka0007 <7takacode@gmail.com>
2023-07-14 10:57:05 +0900

    sol-19

--- For-Univ/information processing theory/task/11th-task/19-sol.m
+++ For-Univ/information processing theory/task/11th-task/19-sol.m
@@ -1,30 +1,38 @@
 % 2つの数値ベクトルが与えられる
 % 相関係数を返す
 
+format long
+
 % --------------------------------------
 % 入力部分
-% 1-77までのランダムな数を持つ数ベクトル(777個の要素を持つ)を生成
-A = rand(1, 777) * randi([2,77]);
-B = rand(1, 777) * randi([2,77]);
+% 1-77までのランダムな数を持つ数ベクトル(7777個の要素を持つ)を生成
+A = rand(1, 7777) * randi([2,77]);
+B = rand(1, 7777) * randi([2,77]);
 % --------------------------------------
 
 % --------------------------------------
 % 処理部分
+n = length(A);
 
-Ans = (vec - mean(vec)) ./ (std(vec));
+% まずは共分散を求める
+SUM = 0;
+for i = 1:n
+    SUM = SUM + (A(i)-mean(A))*(B(i)-mean(B));
+end
+covariance = (SUM/n) 
+
+% 相関係数の導出
+correlation_coefficient = covariance / (std(A)*std(B));
 disp('解答');
-disp('平均が0、標準偏差が1になれば正しい');
-disp('平均');
-disp(mean(Ans));
-disp('標準偏差');
-disp(std(Ans));
+disp(correlation_coefficient);
 % --------------------------------------
+
 
 % --------------------------------------
 % 検算用
-disp('検算用');
-disp('平均');
-disp(mean(zscore(vec)));
-disp('標準偏差');
-disp(std(zscore(vec)));
+coeff = corrcoef(A,B);
+disp('検算');
+disp(coeff(1,2));
+disp('誤差');
+disp(abs(correlation_coefficient-coeff(1,2)));
 % --------------------------------------
